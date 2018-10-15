2bb8aa58836ce2751c44d0fe93617be74d9a1888
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-10-15 17:03:49 +0200

    apply wdc branding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1 class="mat-h1">Developer Open Space 2018</h1>
+    <h1 class="mat-h1">WDC 2018</h1>
     <router-outlet></router-outlet>
 	`
 })
